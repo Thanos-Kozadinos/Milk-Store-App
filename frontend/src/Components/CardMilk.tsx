5fcf27b1c7e9b7ca12e3cef1cde0325ebd9ce12b
Thanos-Kozadinos <akozadinos@hotmail.com>
2023-05-29 23:59:47 +0200

    added some css

--- frontend/src/Components/CardMilk.tsx
+++ frontend/src/Components/CardMilk.tsx
@@ -1,5 +1,6 @@
 import { milks } from "../Services/api"
 import '../css/CardMilk.css'
+import milkImage from '../images/milk.png';
 
 type CardMilkProps = {
     data: milks
@@ -8,10 +9,15 @@ type CardMilkProps = {
 export const CardMilk = ({data}:CardMilkProps) => {
   
     return(
-        <section className="card">        
-            <div>{data.name}</div>
-            <div>{data.type}</div>
-            <div>{data.storage}</div>      
+        <section className="card"> 
+            <div>
+                <img src={milkImage} alt="milk" />
+            </div>
+            <div className="card-info">
+                <h4>{data.name}</h4>
+                <div>{data.type}</div>
+                <div>{data.storage} liter</div>      
+            </div>       
         </section>
     )
 }
